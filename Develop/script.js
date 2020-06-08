7c95a1b8f586b34d2acb0ade75374ad04fc02a40
Blackbird <ksilva.rod@gmail.com>
2020-06-08 18:51:33 -0500

    final hw

--- Develop/script.js
+++ Develop/script.js
@@ -38,9 +38,8 @@ $(document).ready(function() {
   
     //Show the current hour
     var timeNow = parseInt(moment().format('HH'));
-  
-    alert(timeNow);
-    
+
+    //Clears local storage and clears the calendar  
     function clear() {
         localStorage.clear();
   
@@ -55,92 +54,73 @@ $(document).ready(function() {
         box12.val('');
     }
   
+    //Shows current date and time in the top of the calendar and append this at the end of the jumbotron
     function tick() {
-  
         dayEl.text(moment().format('LL'));
         timeEl.text(moment().format('hh:mm:ssA'));
         $('.jumbotron').append(dayEl);
         $('.jumbotron').append(timeEl);
-  
     }
-  
+
+    //Compares each hour with the current hour and changes color of textbox accordingly
+    //Green means future
+    //Red means now
+    //Grey means past
     function checkTime() {
   
         if (parseInt(box12.attr('data-hour')) > timeNow) {
             box12.removeClass('past present future');
             box12.addClass('future');
-  
         } else if (parseInt(box12.attr('data-hour')) === timeNow) {
             box12.removeClass('past present future');
             box12.addClass('present');
-  
-  
         } else {
-  
             box12.removeClass('past present future');
             box12.addClass('past');
-  
         }
-  
+          
         if (parseInt(box1.attr('data-hour')) > timeNow) {
             box1.removeClass('past present future');
             box1.addClass('future')
-  
         } else if (parseInt(box1.attr('data-hour')) === timeNow) {
             box1.removeClass('past present future');
             box1.addClass('present');
-  
-  
         } else {
-  
             box1.removeClass('past present future');
             box1.addClass('past');
-  
         }
   
         if (parseInt(box2.attr('data-hour')) > timeNow) {
             box2.removeClass('past present future');
             box2.addClass('future');
-  
         } else if (parseInt(box2.attr('data-hour')) === timeNow) {
             box2.removeClass('past present future');
             box2.addClass('present');
-  
-  
         } else {
-  
             box2.removeClass('past present future');
             box2.addClass('past');
-  
         }
   
         if (parseInt(box3.attr('data-hour')) > timeNow) {
             box3.removeClass('past present future');
             box3.addClass('future');
-  
         } else if (parseInt(box3.attr('data-hour')) === timeNow) {
             box3.removeClass('past present future');
             box3.addClass('present');
-  
-  
         } else {
             box3.removeClass('past present future');
             box3.addClass('past');
-  
         }
   
         if (parseInt(box4.attr('data-hour')) > timeNow) {
             box4.removeClass('past present future');
             box4.addClass('future');
-  
         } else if (parseInt(box4.attr('data-hour')) === timeNow) {
             box4.removeClass('past present future');
             box4.addClass('present');
-  
         } else {
             box4.removeClass('past present future');
             box4.addClass('past');
-  
         }
   
         if (parseInt(box5.attr('data-hour')) > timeNow) {
@@ -149,12 +129,9 @@ $(document).ready(function() {
         } else if (parseInt(box5.attr('data-hour')) === timeNow) {
             box5.removeClass('past present future');
             box5.addClass('present');
-  
-  
         } else {
             box5.removeClass('past present future');
             box5.addClass('past');
-  
         }
   
         if (parseInt(box9.attr('data-hour')) > timeNow) {
@@ -163,12 +140,9 @@ $(document).ready(function() {
         } else if (parseInt(box9.attr('data-hour')) === timeNow) {
             box9.removeClass('past present future');
             box9.addClass('present');
-  
-  
         } else {
             box9.removeClass('past present future');
             box9.addClass('past');
-  
         }
   
         if (parseInt(box10.attr('data-hour')) > timeNow) {
@@ -177,33 +151,30 @@ $(document).ready(function() {
         } else if (parseInt(box10.attr('data-hour')) === timeNow) {
             box10.removeClass('past present future');
             box10.addClass('present');
-  
-  
         } else {
             box10.removeClass('past present future');
             box10.addClass('past');
-  
         }
-  
+
         if (parseInt(box11.attr('data-hour')) > timeNow) {
             box11.removeClass('past present future');
             box11.addClass('future');
         } else if (parseInt(box11.attr('data-hour')) === timeNow) {
             box11.removeClass('past present future');
             box11.addClass('present');
-  
-  
         } else {
             box11.removeClass('past present future');
             box11.addClass('past');
-  
         }
-  
     }
-  
+
+    //Shows current date and time at top
     tick();
+    //Update current time every second and display it
     setInterval(tick, 1000);
+    //Update colors correctly every 10 minutes
     setInterval(checkTime, 600000);
+    //When Clear button is pushed, it cleans out calendar
     clearBtn.on('click', clear);
   
     button9.on('click', function() {
@@ -218,23 +189,19 @@ $(document).ready(function() {
         if (box10.val() !== '') {
             localStorage.setItem('Entry10', box10.val());
         }
-  
     });
   
     button11.on('click', function() {
         event.preventDefault();
         if (box11.val() !== '') {
             localStorage.setItem('Entry11', box11.val());
-  
         }
-  
     });
   
     button12.on('click', function() {
         event.preventDefault();
         if (box12.val() !== '') {
             localStorage.setItem('Entry12', box12.val());
-  
         }
     });
   
@@ -242,47 +209,35 @@ $(document).ready(function() {
         event.preventDefault();
         if (box1.val() !== '') {
             localStorage.setItem('Entry1', box1.val());
-  
         }
-  
     });
   
     button2.on('click', function() {
         event.preventDefault();
         if (box2.val() !== '') {
             localStorage.setItem('Entry2', box2.val());
-  
         }
-  
     });
   
     button3.on('click', function() {
         event.preventDefault();
         if (box3.val() !== '') {
             localStorage.setItem('Entry3', box3.val());
-  
         }
-  
     });
   
     button4.on('click', function() {
         event.preventDefault();
-  
         if (box4.val() !== '') {
             localStorage.setItem('Entry4', box4.val());
-  
         }
-  
     });
     
     button5.on('click', function() {
         event.preventDefault();
-  
         if (box5.val() !== '') {
             localStorage.setItem('Entry5', box5.val());
-  
         }
-  
     });
   
     checkTime();
